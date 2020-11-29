283504ab3acd2be822363896ff843da2ff07c82c
Satrio <satriotol69@gmail.com>
2020-11-29 18:40:18 +0700

    layouting

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<body>
-    @foreach ($products as $product)
-    {{$product -> categories}}
-    {{$product -> name}}
-    {{$product -> price}}
-    @foreach(json_decode($product->image, true) as $image)
-    <div @if ($loop->first) style="background-color:black" @endif>
+@extends('layouts.app')
+@section('content')
+<style>
+    .section{
+        background-color: black;
+    }
+    .section .container{
+        background-color: white
+    }
+</style>
+<div class="section py-5">
+    <div class="container">
+        
+        @foreach ($products as $product)
+        {{$product -> categories}}
+        {{$product -> name}}
+        {{$product -> price}}
+        @foreach(json_decode($product->image, true) as $image)
         <img src="{{ Voyager::image($image) }}" />
+        @endforeach
+        @endforeach
     </div>
-    @endforeach
-    @endforeach
-</body>
-
-</html>
+</div>
+@endsection
