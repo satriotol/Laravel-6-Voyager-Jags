7f0465e85504212ea8f14b608f0879586f8582ad
Satrio <satriotol69@gmail.com>
2020-12-15 21:09:35 +0700

    database seeder done bro

--- database/seeds/CityTableSeeder.php
+++ database/seeds/CityTableSeeder.php
@@ -12,7 +12,7 @@ class CityTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::insert("INSERT INTO 'cities' ('id', 'province_id', 'name', 'type', 'postal_code', 'created_at', 'updated_at') VALUES
+        DB::insert("INSERT INTO cities (id, province_id, name , type , postal_code, created_at, updated_at) VALUES
         (1, 21, 'Aceh Barat', 'Kabupaten', '23681', '2019-08-29 12:55:53', '2019-08-29 12:55:53'),
         (2, 21, 'Aceh Barat Daya', 'Kabupaten', '23764', '2019-08-29 12:55:53', '2019-08-29 12:55:53'),
         (3, 21, 'Aceh Besar', 'Kabupaten', '23951', '2019-08-29 12:55:53', '2019-08-29 12:55:53'),
@@ -513,7 +513,6 @@ class CityTableSeeder extends Seeder
         (498, 10, 'Wonosobo', 'Kabupaten', '56311', '2019-08-29 12:55:54', '2019-08-29 12:55:54'),
         (499, 24, 'Yahukimo', 'Kabupaten', '99041', '2019-08-29 12:55:54', '2019-08-29 12:55:54'),
         (500, 24, 'Yalimo', 'Kabupaten', '99481', '2019-08-29 12:55:54', '2019-08-29 12:55:54'),
-        (501, 5, 'Yogyakarta', 'Kota', '55111', '2019-08-29 12:55:54', '2019-08-29 12:55:54');
-        ");
+        (501, 5, 'Yogyakarta', 'Kota', '55111', '2019-08-29 12:55:54', '2019-08-29 12:55:54');");
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(ProvinceTableSeeder::class);
-        $this->call(CityTableSeeder::class);
-        // $this->call(DistrictTableSeeder::class);
+        // $this->call(CityTableSeeder::class);
+        $this->call(DistrictTableSeeder::class);
     }
 }
