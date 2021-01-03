50a0f32eb66fcea95a017e8a610209aa0a8755e4
Satrio <satriotol69@gmail.com>
2021-01-03 20:15:34 +0700

    mditrans controller

--- app/Http/Controllers/MidtransController.php
+++ app/Http/Controllers/MidtransController.php
@@ -14,7 +14,23 @@ class MidtransController extends Controller
         \Midtrans\Config::$isProduction = false;
         \Midtrans\Config::$isSanitized = true;
         \Midtrans\Config::$is3ds = true;
-        return view('coba');
+        $params = array(
+            'transaction_details' => array(
+                'order_id' => rand(),
+                'gross_amount' => 10000,
+            ),
+            'customer_details' => array(
+                'first_name' => 'budi',
+                'last_name' => 'pratama',
+                'email' => 'budi.pra@example.com',
+                'phone' => '08111222333',
+            ),
+            'enabled_payments' => ["bca_va", "bni_va","shopeepay"]
+        );
+        
+        $snapToken = \Midtrans\Snap::getSnapToken($params);
+        // echo "snapToken = ".$snapToken;
+        return view('coba',['snapToken' => $snapToken]);
     }
     public function token() 
     {
